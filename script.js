e1037e9cf851fe19b18566990c2a6c1aedca0a97
Emmanuel Nunez <emmanuel@Emmanuels-MacBook-Air.local>
2022-03-07 21:10:07 -0500

    fixed

--- script.js
+++ script.js
@@ -49,62 +49,65 @@ class Deck {
   }
 }
 
-runGame = () => {
-  while (player1.cards.length && player2.length !== 0) {
-    if (player1.cards[0].value > player2.cards[0].value) {
-      player1.cards.push(player1.cards.shift());
-      player1.cards.push(...player2.cards.shift());
-      console.log(player1.cards[0], player2.cards[0])
-    } else if (player2.cards[0].value > player1.cards[0].value) {
-      player2.cards.push(player1.cards.shift());
-      player2.cards.push(...player1.cards.shift());
-      console.log(player2.cards[0], player1.cards[0])
-    } else if (player1.cards[0].value === player2.cards[0].value) {
-      console.log(player1.cards[0], player2.cards[0])
-      declareWar()
-    } if (player2.deck.length === 0) {
-      console.log("Player 1 wins!")
-    } else if (player2.deck.length === 0) {
-      console.log("Player 2 wins!")
-    }
-  }
-  let warDeck = []
-  declareWar = () => {
-    if (player1.cards.length < 4 || player2.cards.length < 4) {
-      if (player1.cards.length = 4) {
-        return
-      } else {
-        player2.cards.length = 0
-        return
-      }
-    } else if (player1.cards[3].value > player2.cards[3].value) {
-      warDeck = player1.cards.splice(0, 4)
-      warDeck.push(...player2.cards.splice(0, 4))
-      player1.cards.push(warDeck)
-      console.log(player1.cards[0], player2.cards[0])
-    } else if (player2.cards[3].value > player1.cards[3].value) {
-      warDeck = player2.cards.splice(0, 4)
-      warDeck.push(...player1.cards.splice(0, 4))
-      player2.cards.push(warDeck)
-      console.log(player2.cards[0], player1.cards[0])
-    } else if (player1.cards[3].value === player2.cards[3].value) {
-      warDeck = player1.cards.splice(0, 4)
-      warDeck.push(...player2.splice(0, 4))
-      console.log(player1.cards[0], player2.cards[0])
-      declareWar()
-    }
-  }
-}
-
 let currentDeck = new Deck
 let player1 = new Player('player1', currentDeck.cards.splice(0, 26))
 let player2 = new Player('player2', currentDeck.cards.splice(0))
-let play1 = player1.deck.shift()
-let play2 = player2.deck.shift()
-console.log(player1.deck[0])
-console.log(player2.deck[0])
+
+const runGame = () => {
+  while (player1.deck.length !== 0 && player2.deck.length !== 0) {
+    if (player1.deck[0].value > player2.deck[0].value) {
+      let array = [player1.deck.shift(), player2.deck.shift()]
+      player1.deck.push(...array);
+      console.log(player1.deck[0], player2.deck[0])
+      console.log("player 1 wins", player1.deck.length, player2.deck.length)
+    } else if (player2.deck[0].value > player1.deck[0].value) {
+      let array = [player1.deck.shift(), player2.deck.shift()]
+      player2.deck.push(...array);
+      console.log(player1.deck[0], player2.deck[0])
+      console.log("player 2 wins", player1.deck.length, player2.deck.length);
+    } else if (player1.deck[0].value === player2.deck[0].value) {
+      console.log(player1.deck[0], player2.deck[0])
+      console.log("tie", player1.deck.length, player2.deck.length)
+      if (player1.deck.length < 5) {
+        player1.deck.length = 0
+        console.log(player1.deck, player2.deck)
+        console.log("Player 2 wins the whole game!")
+        return
+      } else if (player2.deck.length < 5) {
+        player2.deck.length = 0
+        console.log("Player 1 wins the whole game!")
+        return
+      } else {
+        declareWar()
+      }
+    }
+  }
+  if (player2.deck.length === 0) {
+    console.log("Player 1 wins the whole game!")
+  } else if (player1.deck.length === 0) {
+    console.log("Player 2 wins the whole game!")
+  }
+}
+
+let warDeck = []
+let declareWar = () => {
+  // if player 1 or 2 have less than 4 cards
+  let temp = player1.deck.splice(0, 4)
+  let temp2 = player2.deck.splice(0, 4)
+  warDeck.push(...temp, ...temp2)
+  if (player1.deck[0].value > player2.deck[0].value) {
+    player1.deck.push(...warDeck)
+    warDeck.length = 0
+    console.log(player1.deck[0], player2.deck[0])
+  } else if (player2.deck[0].value > player1.deck[0].value) {
+    player2.deck.push(...warDeck)
+    warDeck.length = 0
+    console.log(player1.deck[0], player2.deck[0])
+  } else if (player1.deck[0].value === player2.deck[0].value) {
+    console.log(player1.deck[0], player2.deck[0])
+    declareWar()
+  }
+}
+
+
 runGame()
-// console.log(player1)
-// console.log(player2)
-//console.log(player1.deck.shift())
-//console.log(player2.deck.shift())
